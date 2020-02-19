33cf50a141cf3391fa0aff014aa5ca7f0d46562b
Pranshika Singla <Pranshika_Singla@epam.com>
2020-02-19 15:25:04 +0530

    initial

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PageStages
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+
+        }
+        protected void Page_Init(object sender, EventArgs e)
+        {
+
+        }
+        protected void Page_InitComplete(object sender, EventArgs e)
+        {
+
+        }
+        protected void Page_PreLoad(object sender, EventArgs e)
+        {
+
+        }
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+
+        }
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+
+        }
+        protected void Page_Render(object sender, EventArgs e)
+        {
+
+        }
+        protected void Page_UnLoad(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
